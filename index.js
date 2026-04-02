0545d65132bb86120cef684f9b0c15ffd564440e
babyboy198331-coder <babyboy198331@gmail.com>
2026-04-02 14:34:33 -0500

    master

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 function renderBooks(filter) {
     const booksWrapper = document.querySelector('.books');
-    
- const books = getBooks();
+    const books = getBooks();
 
-if (filter === "Low_To_high") {
-    books.sort((a, b) => a.originalPrice - b.originalPrice);
-}
-else if (filter === "HIGH_TO_LOW") {
-    books.sort((a, b) => b.originalPrice - a.originalPrice);
-}
+    if (filter === "Low_To_high") {
+        books.sort((a, b) => a.originalPrice - b.originalPrice);
+    } else if (filter === "HIGH_TO_LOW") {
+        books.sort((a, b) => b.originalPrice - a.originalPrice);
+    } else if (filter === "RATING") {
+        books.sort((a, b) => b.rating - a.rating);
+    }
+
     const booksHtml = books.map(book => {
         return `<div class="book">
               <figure class="book__img--wrapper">
@@ -28,19 +29,16 @@ else if (filter === "HIGH_TO_LOW") {
             <span class="book__price--normal">$${book.originalPrice.toFixed(2)}</span> $${book.salePrice.toFixed(2)}
         </div>
         </div>`;
-    })
-.join("");
-
+    }).join("");
 
     booksWrapper.innerHTML = booksHtml;
 }
 
 function filterBooks(event) {
-     renderBooks(event.target.value);
-    }
+    renderBooks(event.target.value);
+}
 
-    setTimeout(() => {
-        renderBooks();
-    });
-
+setTimeout(() => {
+    renderBooks();
+});
 
